0009e2410307a6b4a932cd0c43ccf898b14efe7c
zeryx <1892175+zeryx@users.noreply.github.com>
2023-02-18 13:06:24 -0500

    functional commit

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const getAccessToken = require("./sumsub");
+const TenderlySimulationClient = require("./simulation")
 const axios = require("axios");
 const cors = require("cors");
 const serverless = require('serverless-http');
@@ -7,11 +8,12 @@ const serverless = require('serverless-http');
 
 const app = express();
 const router = express.Router();
+const simulationClient = new TenderlySimulationClient()
 
 const ORIGIN = {
     dev: 'http://localhost:8080',
-    test: 'https://dynamic-sable-48c1d8.netlify.app',
-    prod: 'https://gov.weavr.org'
+    test: 'https://test.weavr.org',
+    prod: 'https://www.weavr.org'
 };
 
 
@@ -23,11 +25,19 @@ router.options("/*", cors(), function(req, res, next){
     res.header('Access-Control-Allow-Headers', 'x-app-access-ts,x-app-access-sig ');
     res.sendStatus(200);
 });
-// router.options("/prod/*", cors(), function(req, res, next){
-//     res.header('Access-Control-Allow-Origin', ORIGIN.prod);
-//     res.header('Access-Control-Allow-Headers', 'x-app-access-ts,x-app-access-sig ');
-//     res.sendStatus(200);
-// });
+
+router.post("/simulate-proposal", cors(), async (req, res) => {
+    console.log("Simulation")
+    const proposalId = req.params.proposalId
+    const assetId = req.params.assetId
+    const queueTimestamp = req.params.queueTimestamp
+    const completeTimestamp = req.params.completeTimestamp
+    const networkId = req.params.networkId
+    const response = await simulationClient.simulateCurrentProposal(proposalId, assetId, networkId, queueTimestamp, completeTimestamp)
+    res.status(200).json(response)
+
+})
+
 
 router.get("/dev", cors(), (req, res)=> {
     console.log("DEV")
